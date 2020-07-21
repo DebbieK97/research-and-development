8395ba4a0e8389b83e0f2ad5a8fd0fe49d7dd1a4
DebbieK97 <deeebz.97@gmail.com>
2020-07-21 23:57:11 +0100

    Colour has been added

--- first attempt.R
+++ first attempt.R
@@ -6,7 +6,7 @@ sd(PERatio)
 sd(RSRatio)
 hist(Research$PERatio)
 hist(Research$RSRatio)
-plot(x,y,xlab="PERatio",ylab="RSRatio", main="Research and development")
+plot(x,y,xlab="PERatio",ylab="RSRatio", main="Research and development", col="red")
 x<-PERatio
 y<-RSRatio     
 line(x,y)
